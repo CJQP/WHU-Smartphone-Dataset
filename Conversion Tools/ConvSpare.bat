152156e9cae5ed54d35fe99a95550f2fc1a1ef9e
CJQP <CJQP@users.noreply.github.com>
2024-05-03 22:58:08 +0800

    Add files via upload
    modify ConvSpare bat file

--- Conversion Tools/ConvSpare.bat
+++ Conversion Tools/ConvSpare.bat
@@ -1,10 +1,25 @@
 @echo off
-dir /b *.txt > list
-for /f "tokens=1,2 delims=." %%a in (list) do (
-	 ..\..\..\ExTxPos.exe %%a.%%b.txt
-	 ..\..\..\gga2pos.exe -in %%a.%%b.pos -out %%a.%%b-1.pos -ts 2020/09/15
-	 del %%a.%%b.pos	 
-	 ..\..\..\pos2kml.exe %%a.%%b-1.pos
+
+dir /b *spare*.txt > list
+
+rem Extract Year, Month and Day
+for /f "tokens=1-3 delims=_" %%a in (list) do (
+	set YEAR=%%a
+    set MONTH=%%b
+    set DAY=%%c
 )
 
+
+for /f "tokens=1,2 delims=." %%a in (list) do (
+	rem Extract NMEA[$GPGGA] results(Chipset solutions) from *_spare_log.txt
+	ExTxPos.exe %%a.%%b.txt
+	rem Rename file to "nmea.pos"
+	ren "%%a.%%b.pos" "%%a.%%b_nmea.txt"
+	
+	rem Convert gga file (from .txt to .pos)
+	gga2pos.exe -in *nmea.txt -out %%a.%%b_nmea.pos -ts %YEAR%/%MONTH%/%DAY%
+	
+	rem  Convert .pos file to .kml file	 
+	pos2kml.exe %%a.%%b_nmea.pos
+)
 
